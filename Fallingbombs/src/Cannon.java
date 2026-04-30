c4f968e4038c0f548065a8a306bf3ba0edb8d1d4
Federico-Parolo <federico.parolo@itiszuccante.edu.it>
2026-04-30 12:14:11 +0200

    minor changes + docs beginning not included

--- Fallingbombs/src/Cannon.java
+++ Fallingbombs/src/Cannon.java
@@ -10,15 +10,43 @@ public class Cannon implements Drawable{
 
     public Cannon(int x,int y) {
         currentX = x;
-        currentY = y;
+        currentY = y - 10;
     }
 
 
     public void drawSprite(Graphics2D g2d) {
 
-        g2d.setColor(new Color(0,0,0));
-        g2d.fillRect(currentX,currentY- HEIGHT /2, WIDTH, HEIGHT);
+        //g2d.setColor(new Color(0,0,0));
+        //g2d.fillRect(currentX,currentY- HEIGHT /2, WIDTH, HEIGHT);
+
+        // center
+        g2d.setColor(new Color(180, 180, 180));
+        g2d.fillRect(currentX + 4, currentY, 17, 22);
+
+        // top
+        g2d.setColor(new Color(200, 0, 0));
+        g2d.fillRect(currentX + 8, currentY - 3, 9, 3);
+        g2d.fillRect(currentX + 9, currentY - 6, 7, 3);
+
+        // wings
+        g2d.fillRect(currentX, currentY + 12, 6, 10);
+        g2d.fillRect(currentX + 19, currentY + 12, 6, 10);
+
+        g2d.fillRect(currentX + 2, currentY + 10, 3, 3);
+        g2d.fillRect(currentX + 20, currentY + 10, 3, 3);
+
+        g2d.setColor(new Color(0, 150, 255));
+        g2d.fillRect(currentX + 9, currentY + 6, 7, 6);
+
+        // trail
+        g2d.setColor(Color.ORANGE);
+        g2d.fillRect(currentX + 10, currentY + 22, 3, 6);
+        g2d.fillRect(currentX + 13, currentY + 22, 3, 6);
+
+        g2d.setColor(Color.YELLOW);
+        g2d.fillRect(currentX + 10, currentY + 28, 6, 2);
     }
+
 
     public Projectile fire() {
         return new Projectile(currentX + WIDTH /2 - Projectile.WIDTH/2,currentY);
--- Fallingbombs/src/LobbyPanel.java
+++ Fallingbombs/src/LobbyPanel.java
@@ -194,9 +194,6 @@ public class LobbyPanel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
-                RenderingHints.VALUE_RENDER_QUALITY);
-
         int w = getWidth();
         int h = getHeight();
 
@@ -233,8 +230,6 @@ public class LobbyPanel extends JPanel {
             @Override
             public void paintIcon(Component c, Graphics g, int x, int y) {
                 Graphics2D g2d = (Graphics2D) g.create();
-                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                        RenderingHints.VALUE_ANTIALIAS_ON);
 
                 // background
                 g2d.setColor(new Color(40, 40, 80));
